a6bc0628a195cbf0584bcbeccb7466c46c2e2faf
Adrian Mocanu <mocanuadr90@gmail.com>
2019-01-24 12:47:12 +0200

    First commit

---
+++ SignalRApp/Scripts/SignalR.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    $.connection.hub.start()
+    .done(function () {
+        $.connection.myHub.server.announce("Hello. I'm connected; ");
+    })
+        .fail(function () { alert("Connection failed!"); });
+
+    $.connection.myHub.client.announce = function (message) {
+        $("#msg").append(message + '<br/>');
+    }
+})()
